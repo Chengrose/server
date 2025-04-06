08fbbd2060fd6e04bf7d8131e740049060c25122
Chengrose <3485605154@qq.com>
2025-04-06 15:49:38 +0800

    feat(db): add database configuration

--- app/db.py
+++ app/db.py
@@ -2,7 +2,15 @@ from .models import *
 from sqlmodel import create_engine, Session
 from .config import DATABASE_URL
 
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(
+    DATABASE_URL,
+    echo = True,
+    connect_args = {
+        "use_unicode": True,
+        "charset": "utf8mb4",
+        "init_command": "SET time_zone = '+08:00';",
+    }
+)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
